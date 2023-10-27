15fa8579ebe94a810acba96ac1e3ab643cb9dac4
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-10-27 17:17:02 +0900

    feat: drawer slide button

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,18 +1,30 @@
 import 'react-native-gesture-handler';
 
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
 import {Routes} from './Routes';
 import Home from '../screens/Home/Home';
 import Profile from '../screens/Profile/Profile';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainMenuNavigation = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name={Routes.Home} component={Home} />
+    </Drawer.Navigator>
+  );
+};
 
 const MainNavigation = () => {
   return (
     <Stack.Navigator
       screenOptions={{header: () => null, headerShown: false}}
       initialRouteName={Routes.Home}>
-      <Stack.Screen name={Routes.Home} component={Home} />
+      {/* <Stack.Screen name={Routes.Home} component={Home} /> */}
+      <Stack.Screen name={'Drawer'} component={MainMenuNavigation}/>
       <Stack.Screen name={Routes.Profile} component={Profile} />
     </Stack.Navigator>
   );
